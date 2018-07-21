2f1249af9684ab29b116b204ac04cbe2bde47233
Frank Wunderlich <frank-w@public-files.de>
2018-07-21 17:03:05 +0200

    added build-script

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+TARGET_PRODUCT="bpi-r2"
+BOARD=BPI-R2-720P
+board="bpi-r2"
+BPISOC=mtk
+
+#./configure $BOARD
+
+#if [ -f env.sh ] ; then
+#	. env.sh
+#fi
+
+#make u-boot
+
+CROSS_COMPILE=arm-linux-gnueabihf-
+U_CROSS_COMPILE=$CROSS_COMPILE
+J=$(grep ^processor /proc/cpuinfo  | wc -l )
+
+U_O_PATH=u-boot-mt
+U_CONFIG_H=$U_O_PATH/include/config.h
+UBOOT_CONFIG=mt7623_evb_config
+
+logfile="$(dirname $0)/build.log"
+exec 3> >(tee $logfile)
+ret=1
+
+make -C u-boot-mt $UBOOT_CONFIG CROSS_COMPILE=$U_CROSS_COMPILE -j$J 2>&3 &&
+make -C u-boot-mt CROSS_COMPILE=$U_CROSS_COMPILE -j$J 2>&3 && ret=0
+exec 3>&-
+if [ "$ret" -eq "0" ];
+then
+  echo -e "\033[32m Build success!\033[0m"
+else
+  echo -e "\033[31m Build failed!\033[0m"
+fi
